86bed591ed7b8d711636ed93f3c0a9ccbe2ea5f6
vuzhvlad <vuzh.vlad@gmail.com>
2024-03-27 16:52:56 +0100

    creating simple and dynamic routing for the project, fixing bugs in template product

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ const dataObj = JSON.parse(data); // parsing json into an object
 
 //we created a server by .createServer and passed a callback function that hits every time when request sends to a server
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true); // getting a query and a path name of our url
 
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, {
       // sending header
       "Content-type": "text/html",
@@ -79,14 +79,20 @@ const server = http.createServer((req, res) => {
       })
       .join("");
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
-    console.log(output);
 
     res.end(output);
-  } else if (pathName === "/product") {
-    res.end("This is the product!");
-  } else if (pathName === "/api") {
+  } else if (pathname === "/product") {
     res.writeHead(200, {
-      // sending header
+      "Content-type": "text/html",
+    });
+
+    const product = dataObj[query.id];
+
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+  } else if (pathname === "/api") {
+    res.writeHead(200, {
       "Content-type": "application/json",
     });
     res.end(data);
