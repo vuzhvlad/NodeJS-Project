81e16c09648b9589ec3547ffec40abe18490689e
vuzhvlad <vuzh.vlad@gmail.com>
2024-04-02 11:40:36 +0200

    creating and importing our own module into file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs"); // return object with a lot of functions
 const http = require("http"); // http module, networking capabilites(building https server)
 const url = require("url"); // url module, for working with urls
+const replaceTemplate = require("./modules/replaceTemplate"); // importing our own module
 
 //////////////////////////
 //FILES
@@ -32,21 +33,6 @@ const url = require("url"); // url module, for working with urls
 
 //////////////////////////
 //SERVER
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
